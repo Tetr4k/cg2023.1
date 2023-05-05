7616db3702da111e05d6ef6d4f51ce28aead0e36
Leonardo De Oliveira Carvalho <leonardooc@id.uff.br>
2023-05-05 11:34:51 -0300

    aula 08

---
+++ cg2d11f.cpp
@@ -0,0 +1,24 @@
+#include "Render2D.h"
+#include "matrix.h"
+#include "bezier.h"
+
+int main(){
+	std::vector<vec2> CP = loadCurve("borboleta.txt");
+	std::vector<vec2> P = sample_bezier_spline<3>(CP, 30);
+
+	float t = 0.6;
+	mat2 M = {
+		cos(t), -sin(t),
+		sin(t), cos(t)
+	};
+
+	std::vector<vec2> Q = M*P;
+
+	LineStrip L{P.size()};
+	
+	ImageRGB G(800, 600);
+	G.fill(white);
+	render2d(P, L, red, G);
+	render2d(Q, L, blue, G);
+	G.savePNG("output.png");
+}
